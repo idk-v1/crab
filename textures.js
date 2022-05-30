b617c01181c1979dcf51d2a585c672de9890dcd3
idk-v1 <benhamilton.jpg@gmail.com>
2022-05-29 22:24:43 -0500

    Added Custom Map Support
    No more ugly code to look through

---
+++ textures.js
@@ -0,0 +1,9 @@
+//Player
+let crab1 = new Image(); crab1.onerror = function() {crab1.src = 'textures/missing.png'}; crab1.src = "textures/crab1.png";
+
+//Terrain
+let water1 = new Image(); water1.onerror = function () {water1.src = 'textures/missing.png'}; water1.src = "textures/water2.png";
+let moss1 = new Image(); moss1.onerror = function () {moss1.src = 'textures/missing.png'}; moss1.src = "textures/moss1.png";
+let sand1 = new Image(); sand1.onerror = function () {sand1.src = 'textures/missing.png'}; sand1.src = "textures/sand2.png";
+let grass1 = new Image(); grass1.onerror = function() {grass1.src = 'textures/missing.png'}; grass1.src = "textures/grass2.png";
+let dirt1 = new Image(); dirt1.onerror = function() {dirt1.src = 'textures/missing.png'}; dirt1.src = "textures/dirt2.png";
