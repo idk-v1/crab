1d85888769758d1b1450ed97a8a8860ddc0fa1f9
idk-v1 <104273594+idk-v1@users.noreply.github.com>
2022-05-30 21:21:22 -0500

    Create textures.js

---
+++ textures/textures.js
@@ -0,0 +1,38 @@
+//https://www.piskelapp.com/p/create/sprite
+
+//1 = Up
+//2 = Right
+//3 = Down
+//4 = Left
+
+//Player
+let crab1 = new Image(); crab1.onerror = function() {crab1.src = 'textures/missing.png'}; crab1.src = "textures/crab1.png";
+
+//Terrain
+                                                                                                                                        //00
+
+let water1 = new Image(); water1.onerror = function () {water1.src = 'textures/missing.png'}; water1.src = "textures/water1.png";       //01
+let water2 = new Image(); water2.onerror = function () {water2.src = 'textures/missing.png'}; water2.src = "textures/water2.png";       //02
+let water3 = new Image(); water3.onerror = function () {water3.src = 'textures/missing.png'}; water3.src = "textures/water3.png";       //03
+let water4 = new Image(); water4.onerror = function () {water4.src = 'textures/missing.png'}; water4.src = "textures/water4.png";       //04
+let water5 = new Image(); water5.onerror = function () {water5.src = 'textures/missing.png'}; water5.src = "textures/water5.png";       //05
+
+let sand1 = new Image(); sand1.onerror = function () {sand1.src = 'textures/missing.png'}; sand1.src = "textures/sand1.png";            //06
+let sand2 = new Image(); sand2.onerror = function () {sand2.src = 'textures/missing.png'}; sand2.src = "textures/sand2.png";            //07
+let sand3 = new Image(); sand3.onerror = function () {sand3.src = 'textures/missing.png'}; sand3.src = "textures/sand3.png";            //08
+let sand4 = new Image(); sand4.onerror = function () {sand4.src = 'textures/missing.png'}; sand4.src = "textures/sand4.png";            //09
+                                                                                                                                        //10
+
+let moss1 = new Image(); moss1.onerror = function () {moss1.src = 'textures/missing.png'}; moss1.src = "textures/moss1.png";            //11
+let moss2 = new Image(); moss2.onerror = function () {moss2.src = 'textures/missing.png'}; moss2.src = "textures/moss2.png";            //12
+let moss3 = new Image(); moss3.onerror = function () {moss3.src = 'textures/missing.png'}; moss3.src = "textures/moss3.png";            //13
+let moss4 = new Image(); moss4.onerror = function () {moss4.src = 'textures/missing.png'}; moss4.src = "textures/moss4.png";            //14
+                                                                                                                                        //15
+
+let grass1 = new Image(); grass1.onerror = function() {grass1.src = 'textures/missing.png'}; grass1.src = "textures/grass1.png";        //16
+let grass2 = new Image(); grass2.onerror = function() {grass2.src = 'textures/missing.png'}; grass2.src = "textures/grass2.png";        //17
+let grass3 = new Image(); grass3.onerror = function() {grass3.src = 'textures/missing.png'}; grass3.src = "textures/grass3.png";        //18
+let grass4 = new Image(); grass4.onerror = function() {grass4.src = 'textures/missing.png'}; grass4.src = "textures/grass4.png";        //19
+                                                                                                                                        //20
+
+let dirt1 = new Image(); dirt1.onerror = function() {dirt1.src = 'textures/missing.png'}; dirt1.src = "textures/dirt2.png";             //17
